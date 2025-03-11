186bca8631eddebb2033e757debf3ced764fd92b
Mandeep1612 <mandeep162003@gmail.com>
2025-03-11 11:49:03 +0530

    do annotation base configuration

---
+++ src/main/java/com/SpringLearning/Demo/LifeCycleBeanInterface/AppConfig.java
@@ -0,0 +1,9 @@
+package com.SpringLearning.Demo.LifeCycleBeanInterface;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.SpringLearning.Demo.LifeCycleBeanInterface")
+public class AppConfig {
+}
